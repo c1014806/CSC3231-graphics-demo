908cde45974d535f9e47c06e7dd380f4b7ab7262
elena <orangeelephant@tutanota.com>
2023-12-07 17:22:14 +0000

    Animate particles around obelisk

--- Assets/AnimateObelisk.cs
+++ Assets/AnimateObelisk.cs
@@ -6,9 +6,14 @@ public class AnimateObelisk : MonoBehaviour
 {
     public Transform container;
     public Transform centrePiece;
+    public Transform particle1;
+    public Transform particle2;
+    public Transform particle3;
+    public Transform particle4;
 
     public float animationDuration;
     public float peakIntensity;
+    public float particleHeightMultiplier;
 
     private MeshRenderer obeliskRenderer;
     private float timeElapsed = 0.0f;
@@ -32,7 +37,12 @@ public class AnimateObelisk : MonoBehaviour
         }
         
         animateObeliskMaterialIntensity();
-        container.Rotate(0, 100 * Time.deltaTime, 0);
+        animateParticles();
+        container.Rotate(0, 200 * Time.deltaTime, 0);
+    }
+
+    private void rotateContainer() {
+
     }
 
     private void animateObeliskMaterialIntensity() {
@@ -47,5 +57,21 @@ public class AnimateObelisk : MonoBehaviour
 
         Color color = new Vector4(0.516f*intensity, 0.0f, 0.809f * intensity, intensity);
         obeliskRenderer.material.SetColor("_EmissionColor", color);
+    }
+
+    private void animateParticles() {
+        float radiansThroughRotation = 2 * Mathf.PI / animationDuration * timeElapsed;
+
+        float particle1y = (float) Mathf.Sin(radiansThroughRotation) * particleHeightMultiplier;
+        particle1.localPosition = new Vector3(particle1.localPosition.x, particle1y, particle1.localPosition.z);
+
+        float particle2y = (float) Mathf.Sin(radiansThroughRotation + Mathf.PI / 2) * particleHeightMultiplier;
+        particle2.localPosition = new Vector3(particle2.localPosition.x, particle2y, particle2.localPosition.z);
+
+        float particle3y = (float) Mathf.Sin(radiansThroughRotation + Mathf.PI) * particleHeightMultiplier;
+        particle3.localPosition = new Vector3(particle3.localPosition.x, particle3y, particle3.localPosition.z);
+
+        float particle4y = (float) Mathf.Sin(radiansThroughRotation + Mathf.PI * 3 / 2) * particleHeightMultiplier;
+        particle4.localPosition = new Vector3(particle4.localPosition.x, particle4y, particle4.localPosition.z);
     }
 }
